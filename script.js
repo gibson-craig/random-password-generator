737307196a244ba90577e08753f9c31ef9ff847b
Craig G <gibson.craig05@gmail.com>
2023-08-30 11:35:16 +0100

    further pseudocode planning logic

--- script.js
+++ script.js
@@ -90,17 +90,64 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // length, parseInt (?????), prompt("how many chars") (how to prompt???)
+  // let to store length of password form user input
+  // limitations - if(isNaN(length) === true) (?????)
+  // alert("Password length must be a number.")
+  // return
+
+  // if(length < 10)
+  // alert("......")
+  // return
+
+  // let hasSpecialCharacters = confirm
+  //  "Click OK to ..."
+  // let hasNumberCharacters = confirm
+  //  "Click OK to ..."
+  // let hasLowerChars = confirm
+  //  "Click OK to ..."
+  // let has upperCaseChars = confirm
+  //  "Click OK to ..."
+
+  // if(hasLowerChars = false && (?????)
+  // hasUpperChars = 
+  // hasNumberic = 
+  // hasSpecial = )
+  // alert("Must select at least one character option to generate password")
+  // return
+
+  // let passwordOptions = {
+    // length: length
+    // hasLowerChars: hasLowerChars
+    // hasUpperChars: hasUpperChars
+    // hasNumericChars: hasNumericChars
+    // hasSpecialChars: hasSpecialChars
+  }
+  // console.log(passwordOptions);
+  // return passwordOptions;
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+ // let ... = Math.floor(Math.random)() * arr.length (?????)
+ // let randomElement = arr[randomIndex] (?????)
+ // return randomElement
 }
 
 // Function to generate password with user input
 function generatePassword() {
+// let options = generatePassword();
+// console.log("options");
+// let result = []
 
+
+// let possibleCharacters = []
+
+// let guaranteeCharacters = []
+
+// if(options.hasSpecialChars)
+//  possibleCharacters = possibleCharacters.concat (????) (hasSpecialChars)
 }
 
 // Get references to the #generate element
